795b2041d71e51d8c6d8e36ed16adce97f5a6517
JDR89 <joaquin.romero89@gmail.com>
2025-09-11 11:40:47 -0300

    titulo

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import FormAuthAdmin from "@/components/home/Form_auth_admin";
 export default function Home() {
   return (
     <div className="flex justify-center ">
-
+        <div>SJD</div>
       <div>
         <FormAuthAdmin />
       </div>
